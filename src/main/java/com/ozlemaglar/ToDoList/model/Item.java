95730f18a1a146328928cdd22be5678e4ddbe4c8
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-11-08 20:36:06 +0300

    model

--- src/main/java/com/ozlemaglar/ToDoList/model/Item.java
+++ src/main/java/com/ozlemaglar/ToDoList/model/Item.java
@@ -1,15 +1,40 @@
-package com.ozlemaglar.ToDoList.item;
+package com.ozlemaglar.ToDoList.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 
-public class ToDoItem {
+@Entity
+@Table(name="item")
+
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String todoName;
+
+    @Column
+    @NotNull(message = "task.validation.constraints.NotNull.message")
+    private String title;
+
+    @Column
     private boolean isDone;
+
+    @Column(name = "created_date")
+    @CreatedDate
+    private Date createdDate;
+
+    @Column(name = "update_date")
+    @LastModifiedDate
+    private Date updateDate;
 }
