65ccda91e23e4c1372b97df14defe7e7ca18270e
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-11-14 22:57:34 +0300

    components

---
+++ frontend/src/components/AddTask.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class AddTask extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            description:null,
+
+        }
+    }
+    
+    componentDidMount() {
+        
+    }
+    
+
+  render() {
+    return (
+      <div>AddTask</div>
+    )
+  }
+}
