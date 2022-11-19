b28c4a8e7604025cce4165f8a129e62332d3694f
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-11-19 17:43:58 +0300

    toaster style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,34 @@
-import './App.css'
-import { useState } from 'react';
-import TodoContainer from './components/TodoContainer';
-import TodoList from './components/TodoList';
+import './App.css';
+import TodoInput from "./components/TodoInput";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
+    return (
+        <div className="App">
+            <div className="container">
+                <TodoInput
 
-  const [inputText, setInputText]= useState("");
-  const [todos, setTodos]=useState([]);
-
-  return (
-    <div className="App">
-      <header>
-        <h1>ToDo List</h1>
-      </header>
-      
-      <TodoContainer
-      inputText={inputText}
-      setInputText={setInputText}
-      todos={todos}
-      setTodos={setTodos}
-      />
-
-      <TodoList/>
-      
-    </div>
-  );
+                />
+                <Toaster
+                    position="top-right"
+                    reverseOrder={true}
+                    toastOptions={{
+                        // Define default options
+                        duration: 3000,
+                        style: {
+                            background: '#333',
+                            color: '#fff',
+                            padding: '16px',
+                            width: '275px',
+                            height: '75px',
+                            borderRadius: '10px',
+                            fontSize: '18px'
+                        }
+                    }}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
